a3f774dbae4a059004d13ac30f992288132d6984
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-11-24 14:14:22 -0500

    Added Flip model

---
+++ Flips/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Flip(models.Model):
+    private = models.BooleanField(default=False, null=False)
+
+    option_a = models.CharField(max_length=128, default="Heads", blank=False, null=False)
+    option_a_weight = models.FloatField(default=0.5, null=False)
+
+    option_b = models.CharField(max_length=128, default="Tails", blank=False, null=False)
+    option_b_weight = models.FloatField(default=0.5,  null=False)
+
+    outcome_rating = models.IntegerField(default=0)  # 0 == unrated, 1 == Bad, 2 == Neutral, 3 == Happy
+    comment = models.CharField(max_length=256, default="")
+
