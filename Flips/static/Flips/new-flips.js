964af9584c2d456f6ebcd47d06251a69794dffc8
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2024-11-24 14:51:40 -0500

    Added weighting slider

---
+++ Flips/static/Flips/new-flips.js
@@ -0,0 +1,34 @@
+let option_a_name = document.getElementById("option-a");
+let option_b_name = document.getElementById("option-b");
+
+let weighting_slider = document.getElementById("weighting");
+let old_slider_value = weighting_slider.value;
+let option_a_weight = 0;
+let option_b_weight = 0;
+
+let option_a_weighting_label = document.getElementById("option-a-weighting");
+let option_b_weighting_label = document.getElementById("option-b-weighting");
+
+
+function update_weighting_labels(a, b) {
+    let label_a = `Option A: ${Math.floor(a * 100)}%`;
+    let label_b = `Option B: ${Math.floor(b * 100)}%`;
+
+    option_a_weighting_label.innerText = label_a;
+    option_b_weighting_label.innerText = label_b;
+}
+
+
+setInterval(() => {
+    let slider_value = weighting_slider.value;
+    if (slider_value === old_slider_value) {
+        return;
+    }
+
+    slider_value = slider_value / 100;
+
+    option_a_weight = slider_value;
+    option_b_weight = 1-slider_value;
+
+    update_weighting_labels(option_a_weight, option_b_weight);
+}, 25);
--- Flips/views.py
+++ Flips/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required()
+def new_flip(request):
+    return render(request, "Flips/new-flip.html")
